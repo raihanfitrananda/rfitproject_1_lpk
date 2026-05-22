0580cec5f5689997bac27f0e241ad044c81234c7
raihanfitrananda <raihanfitrananda@gmail.com>
2026-05-22 14:21:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title(" Hi, User Welcome to Rfitproject")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://www.instagram.com/rfitran_/#)."
+    "Let's start building! For help and inspiration, head over to [Rfit.Instagram](https://www.instagram.com/rfitran_/#)."
 )
